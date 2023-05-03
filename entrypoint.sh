973642a67298d72c17712e690319f3a691911674
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2023-05-03 14:59:07 -0300

    Permitindo definir a porta de forma dinâmica

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash --login
 # The --login ensures the bash configuration is loaded, enabling Conda.
 
-# Enable strict mode to run whatever commands...
-set -euo pipefail
-
 # Temporarily disable strict mode and activate conda:
 set +euo pipefail
 conda activate img2mol
@@ -12,6 +9,9 @@ pip install .
 # Re-enable strict mode:
 set -euo pipefail
 
+# If WEB_API_PORT not set or null, set it to 4200.
+WEB_API_PORT="${WEB_API_PORT:=4200}"
+
 # exec the final command:
 export FLASK_APP=/app/web-api.py
-exec flask run -h 0.0.0.0 -p 4200
+exec flask run -h 0.0.0.0 -p $WEB_API_PORT
